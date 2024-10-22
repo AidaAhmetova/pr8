8858d5fb88909988b29c7f9467d5e57c08058208
AidaAhmetova <aidaahmetova13@gmail.com>
2024-10-22 20:27:23 +0500

    first

---
+++ homework3/Aidaa.py
@@ -0,0 +1,12 @@
+def draw_square(size):
+    for _ in range(size):
+        print(' * ' * size)
+
+size_input = input("Введите размер квадрата (целое число): ")
+
+
+if size_input.isdigit() and int(size_input) == 10:
+    size = int(size_input)
+    draw_square(size)
+else:
+    print("Ошибка: размер квадрата должен быть целым числом 10.")
---
+++ homework4/Aidaaa.py
@@ -0,0 +1,21 @@
+def is_integer(n):
+    try:
+        float_n = float(n)
+        return float_n.is_integer()
+    except ValueError:
+        return False
+
+a = input("Введите первое число (a): ")
+b = input("Введите второе число (b): ")
+
+if is_integer(a) and is_integer(b):
+    a = int(a)
+    b = int(b)
+
+    start = min(a, b) + 1
+    end = max(a, b)
+   
+    for number in range(start, end):
+        print(number)
+else:
+    print("Ошибка: оба введенных числа должны быть целыми.")
